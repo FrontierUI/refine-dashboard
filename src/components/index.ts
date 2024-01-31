271523c3539a1f30e518584187afc031728abfcb
FrontierUI <uifrontier95@gmail.com>
2024-01-31 17:52:58 +0500

    usman commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,32 @@
-export { Header } from "./header";
+import DashboardTotalCountCard from './home/total-count-card';
+import UpcomingEvents from './home/upcoming-events';
+import LatestActivities from './home/latest-activities';
+import DealsChart from './home/deals-chart';
+
+import UpcomingEventsSkeleton from './skeleton/upcoming-events';
+import AccordionHeaderSkeleton from './skeleton/accordion-header';
+import KanbanColumnSkeleton from './skeleton/kanban';
+import ProjectCardSkeleton from './skeleton/project-card';
+import LatestActivitiesSkeleton from './skeleton/latest-activities';
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+  DashboardTotalCountCard,
+  LatestActivities,
+};
+
+// export * from './tags/user-tag';
+export * from './text';
+// export * from './accordion';
+// export * from './tasks/form/description';
+// export * from './tasks/form/due-date';
+// export * from './tasks/form/stage';
+// export * from './tasks/form/title';
+// export * from './tasks/form/users';
+// export * from './tasks/form/header';
